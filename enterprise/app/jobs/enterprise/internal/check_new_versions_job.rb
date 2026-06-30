4f51ff0a1d6dd0eb98abe5d1ee5031116bf4e061
zetxtech <i@zetx.tech>
2026-06-30 10:54:05 +0800

    feat(enterprise): honor SKIP_PLAN_SYNC to prevent Hub plan override

--- enterprise/app/jobs/enterprise/internal/check_new_versions_job.rb
+++ enterprise/app/jobs/enterprise/internal/check_new_versions_job.rb
@@ -9,6 +9,7 @@ module Enterprise::Internal::CheckNewVersionsJob
 
   def update_plan_info
     return if @instance_info.blank?
+    return if ActiveModel::Type::Boolean.new.cast(ENV.fetch('SKIP_PLAN_SYNC', false))
 
     update_installation_config(key: 'INSTALLATION_PRICING_PLAN', value: @instance_info['plan'])
     update_installation_config(key: 'INSTALLATION_PRICING_PLAN_QUANTITY', value: @instance_info['plan_quantity'])
